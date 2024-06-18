f074448023ef2feb0c77ce976b874874a1a92b6c
Amidu Dabor <36164952+Amidu-Dabor@users.noreply.github.com>
2024-06-18 04:22:20 +0300

    Update ssh-connect-instance-ids.ps1

--- ssh-connect-instance-ids.ps1
+++ ssh-connect-instance-ids.ps1
@@ -15,7 +15,7 @@ function CreateSSHKeyFile {
     )
 
     # Fetch SSH key from GitHub Secrets
-    $sshPrivateKey = $env:${{sshKeySecretName}}
+    $sshPrivateKey = $env:${sshKeySecretName}
 
     # Write SSH key to file
     Set-Content -Path $sshKeyFilePath -Value $sshPrivateKey
